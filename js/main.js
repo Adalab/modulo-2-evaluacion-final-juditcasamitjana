63add477095fe74a980a88d26dd3a6f63bc6d177
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-03-15 20:40:12 +0100

    image not found

--- js/main.js
+++ js/main.js
@@ -7,16 +7,11 @@ const submitBtn = document.querySelector(".js-submit-btn");
 const resetBtn = document.querySelector(".js-reset-btn");
 const seriesList = document.querySelector(".js-series-list");
 const favoritesList = document.querySelector(".js-favorites-list");
+const wrongImage = "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
+const imageNotFound = "https://fakeimg.pl/225x309?text=Image+not+found&font_size=40";
 
 let series = [];
 let favorites = [];
-
-// Cuando la usuaria escribe en el input:
-//   -escuho el click
-//   -obtener el valor del input
-//   -petición al servidor para buscar en la API
-//   -me duevuelve los resultados
-//   -render
 
 const createSerie = (serie) => {
     return `<li>
@@ -46,10 +41,12 @@ const handleClickSubmit = (e) => {
         .then(response => response.json())
         .then(data => {
             const results = data.data.map(serie => {
+                const originalImage = serie.images.jpg.image_url;
+
                 return {
                     id: serie.mal_id,
                     title: serie.title,
-                    image: serie.images.jpg.image_url,
+                    image: originalImage === wrongImage ? imageNotFound : originalImage,
                     favorite: false,
                 }
             })
