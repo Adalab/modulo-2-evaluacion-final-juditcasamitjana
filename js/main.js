be54a2d3f8d4479ba695ec469d197bc8ec4a9a66
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-03-18 10:06:48 +0100

    add exercices

--- js/main.js
+++ js/main.js
@@ -5,14 +5,17 @@ const submitBtn = document.querySelector(".js-submit-btn");
 const resetBtn = document.querySelector(".js-reset-btn");
 const seriesList = document.querySelector(".js-series-list");
 const favoritesList = document.querySelector(".js-favorites-list");
-const wrongImage = "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
-const imageNotFound = "https://fakeimg.pl/225x309?text=Image+not+found&font_size=40";
+const wrongImage =
+    "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png";
+const imageNotFound =
+    "https://fakeimg.pl/225x309?text=Image+not+found&font_size=40";
+const logBtn = document.querySelector(".js-log-btn");
 
 let series = [];
 let favorites = [];
 
 const createSerie = (serie) => {
-    return `<li class="${isFavorite(serie.id) ? 'favorite': ''}">
+    return `<li class="${isFavorite(serie.id) ? "favorite" : ""}">
                 <div class="container-serie">
                     <div>
                     <img src="${serie.image}"/>
@@ -23,7 +26,7 @@ const createSerie = (serie) => {
                     </div>
                 </div>
             </li>`;
-}
+};
 
 const createFavorite = (serie) => {
     return `<li>
@@ -37,37 +40,37 @@ const createFavorite = (serie) => {
                     </div>
                 </div>
             </li>`;
-}
+};
 
 const renderSeries = (series) => {
     seriesList.innerHTML = "";
-    for(const serie of series) {
+    for (const serie of series) {
         seriesList.innerHTML += createSerie(serie);
     }
-}
+};
 
 const renderFavorites = (series) => {
     favoritesList.innerHTML = "";
-    for(const serie of series) {
+    for (const serie of series) {
         favoritesList.innerHTML += createFavorite(serie);
     }
-}
+};
 
 const loadFavorites = () => {
-    const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
 
     if (!storedFavorites) {
         return;
     }
-    
+
     favorites = storedFavorites;
     renderFavorites(favorites);
-}
+};
 
 const isFavorite = (serieId) => {
-    const serie = favorites.find(serie => serie.id === serieId);
+    const serie = favorites.find((serie) => serie.id === serieId);
     return serie ? true : false;
-}
+};
 
 const handleClickSubmit = (e) => {
     e.preventDefault();
@@ -75,22 +78,25 @@ const handleClickSubmit = (e) => {
     const search = inputSearch.value; //obtengo el valor de input
 
     fetch(`https://api.jikan.moe/v4/anime?q=${search}`)
-        .then(response => response.json())
-        .then(data => {
-            const results = data.data.map(serie => {
+        .then((response) => response.json())
+        .then((data) => {
+            const results = data.data.map((serie) => {
                 const originalImage = serie.images.jpg.image_url;
 
                 return {
                     id: serie.mal_id,
                     title: serie.title,
-                    image: originalImage === wrongImage ? imageNotFound : originalImage,
-                }
-            })
+                    image:
+                        originalImage === wrongImage
+                            ? imageNotFound
+                            : originalImage,
+                };
+            });
 
             series = results;
             renderSeries(series);
-        })
-}
+        });
+};
 
 const handleAddFavorite = (e) => {
     const serieId = e.target.id;
@@ -99,13 +105,13 @@ const handleAddFavorite = (e) => {
         return;
     }
 
-    const serie = series.find(serie => serie.id === parseInt(serieId));
+    const serie = series.find((serie) => serie.id === parseInt(serieId));
     favorites.push(serie);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+    localStorage.setItem("favorites", JSON.stringify(favorites));
 
     renderFavorites(favorites);
     renderSeries(series);
-}
+};
 
 const handleDeleteFavorite = (e) => {
     const serieId = e.target.id;
@@ -114,26 +120,37 @@ const handleDeleteFavorite = (e) => {
         return;
     }
 
-    const newFavorites = favorites.filter(favorite => favorite.id !== parseInt(serieId));
+    const newFavorites = favorites.filter(
+        (favorite) => favorite.id !== parseInt(serieId)
+    );
     favorites = newFavorites;
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+    localStorage.setItem("favorites", JSON.stringify(favorites));
 
     renderFavorites(favorites);
     renderSeries(series);
-}
+};
 
 const handleClickReset = () => {
-    localStorage.removeItem('favorites');
+    localStorage.removeItem("favorites");
     series = [];
     favorites = [];
 
     renderFavorites(favorites);
     renderSeries(series);
-}
+};
+
+const handleClickLog = (e) => {
+    e.preventDefault();
+
+    for (const serie of series) {
+        console.log(serie.title);
+    }
+};
 
 submitBtn.addEventListener("click", handleClickSubmit);
 seriesList.addEventListener("click", handleAddFavorite);
 favoritesList.addEventListener("click", handleDeleteFavorite);
 resetBtn.addEventListener("click", handleClickReset);
+logBtn.addEventListener("click", handleClickLog);
 
 loadFavorites();
